8455e8930fa326a3386fd17a5fb3da09a49ff124
your name hereJustinlf55 <Justinlfern@gmail.com>
2020-03-23 23:05:24 -0700

    boss character

--- src/boss_enemy.js
+++ src/boss_enemy.js
@@ -2,7 +2,14 @@ import Enemy from './enemies';
 
 class BossEnemy extends Enemy {
     constructor(game) {
+        super(game);
+        this.pos = [0, 200];
+        this.img = new Image();
+        this.size = [200, 200];
+        this.damage = 200;
         this.hp = 500;
-        this.damage = 500;
     }
+    
  }
+
+export default BossEnemy;
