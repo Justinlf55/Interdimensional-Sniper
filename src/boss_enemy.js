7cae36111ce8e2217cbf8eb2d3879fa422b023bd
your name hereJustinlf55 <Justinlfern@gmail.com>
2020-03-26 13:09:15 -0700

    worked on boss character

--- src/boss_enemy.js
+++ src/boss_enemy.js
@@ -7,7 +7,7 @@ class BossEnemy extends Enemy {
         this.img = new Image();
         this.size = [200, 200];
         this.damage = 200;
-        this.hp = 555;
+        this.hp = 550;
     }
     
  }
